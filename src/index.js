52b9b09b8e1df789f1bba6a545a7a1f2c0337899
frankycty <frankychantakyu@yahoo.com.hk>
2021-06-19 13:52:03 +1200

    get rid of grapgql and typescript

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import 'reflect-metadata';
+// import redis from 'redis';
+// import connectRedis from 'connect-redis';
+import Mongoose from 'mongoose';
+import { Server } from '@frankycty/node-framework';
+const CONFIG = require('config');
+
+const main = async () => {
+  // ============= Connect with Redis =========================
+
+  // const RedisStore = connectRedis(session);
+  // const redisClient = redis.createClient();
+
+  // ============= Connect with MongoDB =======================
+  const connectionOpts = {
+    promiseLibrary: global.Promise,
+    poolSize: 25,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  };
+
+  await Mongoose.connect(CONFIG.db.uri, connectionOpts);
+
+  const routes = [
+    {
+      method: 'get',
+      path: '/',
+      handler: (ctx, _) => {
+        ctx.body = {
+          status: 'success',
+        };
+      },
+    },
+  ];
+
+  await Server.start(routes, [], { port: CONFIG.server.port });
+};
+
+main();
